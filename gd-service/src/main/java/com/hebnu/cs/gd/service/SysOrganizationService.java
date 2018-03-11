23a09aaea35149138bf8386d8e3e8b025c5ae90e
1039738015@qq.com <1039738015wzf>
2018-03-11 11:09:27 +0800

    修改包名

---
+++ gd-service/src/main/java/com/hebnu/cs/gd/service/SysOrganizationService.java
@@ -0,0 +1,34 @@
+package com.hebnu.cs.gd.service;
+
+
+
+import com.hebnu.cs.gd.model.dto.PageInfo;
+import com.hebnu.cs.gd.model.dto.SysOrganizationTree;
+import com.hebnu.cs.gd.model.entity.SysOrganization;
+
+import java.util.List;
+
+/**
+ * @Author: ouyangan
+ * @Date : 2016/10/10
+ */
+public interface SysOrganizationService {
+    Long insertOrganization(SysOrganization sysOrganization);
+
+    int deleteOrganization(long id);
+
+    void updateOrganization(SysOrganization organization);
+
+    PageInfo selectPage(int page, int row, long id);
+
+    public SysOrganizationTree selectSysOrganizationTree(long id);
+
+    public List<SysOrganizationTree> selectChildrenTreeList(long id);
+
+    boolean isExistFullName(String fullName);
+
+    SysOrganization selectOrganization(long id);
+
+    boolean isExistFullNameExcludeId(long id, String fullName);
+
+}
---
+++ gd-service/src/main/java/com/hebnu/cs/gd/service/SysUserService.java
@@ -0,0 +1,36 @@
+package com.hebnu.cs.gd.service;
+
+import com.hebnu.cs.gd.model.dto.LoginInfo;
+import com.hebnu.cs.gd.model.dto.PageInfo;
+import com.hebnu.cs.gd.model.entity.SysUser;
+
+import java.io.Serializable;
+
+/**
+ * @Author: ouyangan
+ * @Date : 2016/10/7
+ */
+public interface SysUserService {
+
+    long insertUser(SysUser user, String jobIds, String permissionIds);
+
+    boolean isExistLoginName(String loginName);
+
+    void deleteById(long id);
+
+    SysUser selectById(long id);
+
+    boolean isExistLoginNameExcludeId(long id, String loginName);
+
+    void updateUser(SysUser user, String jobIds, String permissionIds);
+
+    PageInfo selectPage(int page, int rows, String sort, String order, String loginName, String zhName, String email, String phone, String address);
+
+    void updateUser(SysUser user);
+
+    SysUser selectByLoginName(String loginName);
+
+    LoginInfo login(SysUser user, Serializable id, int platform);
+
+
+}
